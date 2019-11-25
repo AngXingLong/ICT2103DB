a1508abee6dea0d82d38047a583e755020f31bd2
Nova136 <48143177+Nova136@users.noreply.github.com>
2019-11-25 18:32:50 +0800

    modified conversion data

--- mongoDB/dataConversion.php
+++ mongoDB/dataConversion.php
@@ -1,5 +1,9 @@
 <?php
 
+require "navmenu.php";
+
+getHeader(array(),array(),false);
+
 //https://docs.mongodb.com/php-library/v1.2/reference/method/MongoDBCollection-insertMany/
 require "../mysql/database.php";
 require "mongoDatabase.php";
@@ -38,3 +42,45 @@ $collection->insertMany($json);
 
 
 ?>  
+<html>
+<div class="container">
+  <h2>Modal Example</h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title"></h4>
+        </div>
+        <div class="modal-body">
+            <p>Data updated!</p>
+        </div>
+        <div class="modal-footer">
+          <a href="./index.php" class="btn btn-primary">Confirm</a>
+        </div>
+      </div>
+      	
+    </div>
+  </div>
+  
+</div>
+<script type="text/javascript">
+    $(window).on('load',function(){
+        $('#myModal').modal('show');
+    });
+</script>
+</html>
+
+<style>
+
+#background {
+  padding-left:100px;
+}
+
+</style>
