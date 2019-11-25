99a190fd27cf89819905fcc62ae604f816bec6f7
AngXingLong <angxinglong@gmail.com>
2019-11-25 23:08:27 +0800

    Merged

---
+++ mysql/admin/retrieveFromDB/retrieveAccounts.php
@@ -0,0 +1,75 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+require_once 'dbfunction.php';
+require_once 'data.php';
+
+$db_selected = mysqli_select_db($connection, DB_DB)
+or die("Could not select examples");
+
+$result = mysqli_query($connection, "SELECT * FROM accounts");
+if(mysqli_num_rows($result) > 0){
+    echo'<div class="table-responsive" id="userTable">';
+
+    echo'<table id="example"  cellspacing="0" width="100%" class="table " >';
+    echo'<thead>';
+    echo'<tr>';
+    echo'<th>#</th>';
+    echo'<th>First Name</th>';
+    echo'<th>Last Name</th>';
+    echo'<th>Full Name</th>';
+    echo'<th>Username</th>';
+    echo'<th>Password</th>';
+    echo'<th>Account Type</th>';
+
+
+    echo'</tr>';
+    echo'</thead>';
+
+    echo'<tbody>';
+
+    //Output all rows into table
+    while($rows = mysqli_fetch_assoc($result)){
+        echo'<tr>';
+        echo'<td>'.$rows["account_id"].'</td>';
+        echo'<td>'.$rows["first_name"].'</td>';
+        echo'<td>'.$rows["last_name"].'</td>';
+        echo'<td>'.$rows["first_name"].' '.$rows["last_name"].'</td>';
+        echo'<td>'.$rows["account_name"].'</td>';
+        echo'<td>'.$rows["account_password"].'</td>';
+
+        if($rows["account_type_id"] == '1'){
+            echo "<td>Admin</td>";
+        }else{
+            echo "<td>User</td>";
+        }
+
+        //Edit Button
+        echo'<form action="accounts.php" method="POST">';
+
+        echo'<td> <button class="btn btn-info fa fa-pencil-square-o" id='.$rows["account_id"].' onclick="edit(id)" name="edit" ></button></td>';
+        echo'</form>';
+        
+        //Delete Button
+        echo'<form action="accounts.php" method="POST">';
+        echo'<td><button class="btn btn-danger fa fa-trash-o" name="delete" value="'.$rows["account_id"].'"></button></td>';
+        echo'</form>';
+        
+        echo'</tr>';
+
+    }
+
+    echo'</tbody>';
+    echo'</table>';
+    echo'</div>';
+    close_connection();
+    
+}else{
+    echo '0 Results';
+    close_connection();
+}
+
+
